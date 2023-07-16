7524aa1bb570e70225c0173d00b47bbeca7384b4
John Romkey <58883+romkey@users.noreply.github.com>
2023-07-16 09:42:23 -0700

    delete old example secrets.py

--- examples/secrets.py
+++
@@ -1,9 +0,0 @@
-# SPDX-FileCopyrightText: 2023 John Romkey
-#
-# SPDX-License-Identifier: CC0-1.0
-
-secrets = {
-    "ssid": "YOUR WIFI SSID HERE",
-    "password": "********",
-    "syslogger": "ip-address-of-syslog-server-or-omit"
-    }
